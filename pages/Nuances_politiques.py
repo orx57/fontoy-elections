c8c34ad41b81c2d53884c54c1ee8f35021953044
Olivier Raggi <olivier@raggi.fr>
2024-07-29 22:49:50 +0200

    Update Nuances_politiques.py

--- pages/Nuances_politiques.py
+++ pages/Nuances_politiques.py
@@ -7,14 +7,10 @@ def load_data(url):
     data = pd.read_csv(url)
     return data
 
-def main():
-    st.title("Nuances politiques")
+st.title("Nuances politiques")
     
-    data = load_data(constants.NUANCES_CSV_URL)
-    st.dataframe(data, column_config={'Unnamed: 0': None})
+data = load_data(constants.NUANCES_CSV_URL)
+st.dataframe(data, column_config={'Unnamed: 0': None})
 
-    st.write("Source de données :")
-    st.write(constants.SOURCES_URL)
-
-if __name__ == "__main__":
-    main()
+st.write("Source de données :")
+st.write(constants.SOURCES_URL)
